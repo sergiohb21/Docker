1a96e6ef7fba8e3fa5e30635f71fc1bff8c2eb22
Sergio <125660709+sergiohb21@users.noreply.github.com>
2023-11-08 00:05:34 +0100

    Add files via upload

---
+++ Docker-Myxampp-aarch64/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+$cfg['Servers'][1]['host'] = 'localhost';
+$cfg['Servers'][1]['port'] = '3306';
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = '1234'; // Deja esto como tu contraseña de MySQL
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['AllowNoPassword'] = true;
+?>
+
